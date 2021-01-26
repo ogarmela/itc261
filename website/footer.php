a0cb155b73ee9e0d7fe8bc66e24b45a08a5e134e
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-01-25 21:37:40 -0800

    Add files via upload

---
+++ website/footer.php
@@ -0,0 +1,18 @@
+<footer>
+        <ul>
+         <li>Copyright &copy;
+             <?php
+            echo date('Y');
+              ?>
+            </li> 
+            <li>All Right Reserved</li>
+            <li><a href="../index.php">Portal Page</a></li>
+            <li><a href="">HTML VALID</a></li>
+            <li><a href="">CSS VALID</a></li>
+            
+            
+            </ul>
+          </footer>
+    </div>  <!--end wrapper-->
+</body>
+</html>
