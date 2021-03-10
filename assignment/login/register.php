711aec358f40c2254e1400016f069b6f9d2781de
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-09 22:48:19 -0800

    Add files via upload

---
+++ assignment/login/register.php
@@ -0,0 +1,44 @@
+<?php // My Regiser page - a simple form -- pointing to the server
+// our fields
+//FirstName
+//LastName
+//Email
+//UserName
+//Password
+
+//include('server.php');
+//include('includes/header.php');
+?>
+
+<h1>Register Today</h1>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+<fieldset>
+ <label>First Name</label>   
+<input type="text" name="FirstName" value="<?php if(isset($_POST['FirstName'])) echo htmlspecialchars($_POST['FirstName']) ;?>"> 
+
+<label>Last Name</label>   
+<input type="text" name="LastName" value="<?php if(isset($_POST['LastName'])) echo htmlspecialchars($_POST['LastName']) ;?>"> 
+    
+<label>Email</label>   
+<input type="Email" name="Email" value="<?php if(isset($_POST['Email'])) echo htmlspecialchars($_POST['Email']) ;?>"> 
+
+<label>Username</label>   
+<input type="text" name="UserName" value="<?php if(isset($_POST['UserName'])) echo htmlspecialchars($_POST['UserName']) ;?>"> 
+   
+<label>Your Password</label>
+<input type="password" name="Password_1">
+    
+<label>Confirm Password</label>
+<input type="password" name="Password_2">
+    
+<button type="submit" class="btn" name="reg_user">Register</button>
+ 
+<button type="button" onclick="window.locatiion.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>'">Reset</button> 
+    
+</fieldset>
+</form>
+
+<P class="center"><a href="login.php">Already a member?  Please login!</a></P>
+
+
