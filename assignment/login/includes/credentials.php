2a89b27d8c5babdc42c7ded62ef72b57e2f857b9
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-09 22:45:38 -0800

    Add files via upload

---
+++ assignment/login/includes/credentials.php
@@ -0,0 +1,10 @@
+<?php // credentials.php
+
+<?php // credentials.php
+
+
+    
+define('DB_NAME', 'garmela');
+define('DB_HOST', 'mysql.omar12.com');
+define('DB_USER', 'ogarmela');
+define('DB_PASSWORD', 'Hamdi12345$');
