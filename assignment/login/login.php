0dbf48f3ebb78071bf2a00853ecbad8130512c72
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-11 12:13:26 -0800

    Add files via upload

--- assignment/login/login.php
+++ assignment/login/login.php
@@ -1,1 +1,29 @@
 <?php // log in page will be pointing to the server.php page - username and password
+
+include('server.php');
+//include('includes/header.php');
+
+
+?>
+
+<h1>Login</h1>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+<fieldset>
+<label>Username</label>    
+<input type="text" name="UserName" value="<?php if(isset($_POST['UserName']) echo htmlspecialchars($_POST['UserName'])) ;?>">
+<label>Your Password</label>
+ <input type="password" name="Password">
+    <?php
+    include('includes/errors.php');
+    ?>
+   
+<button type="submit" class="btn" name="Login_user">Login</button>
+    
+<button type="button" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>'">Reset</button>
+    
+</fieldset>
+    
+</form>
+<P class="center">Haven't Registered? <a href="register.php">Register Here</a></P>
+
