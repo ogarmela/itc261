36c2e0c827118d1226fefde78566db7a046c14b8
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-17 11:15:59 -0700

    Add files via upload

--- assignment/login/includes/config.php
+++ assignment/login/includes/config.php
@@ -4,13 +4,11 @@
 ob_start(); // prevents header errors before reading the whole page!
 define('DEBUG', 'TRUE'); // we want to see our errors
 
-include('credentials.php');
+include('includes/credentials.php');
+include('server.php');
 
-
-function myError($myFile, $myLine, $errorMsg)
-{
-if(defined('DEBUG') && DEBUG)
-{
+function myError($myFile, $myLine, $errorMsg) {
+if(defined('DEBUG') && DEBUG) {
  echo 'Error in file: <b> '.$myFile.' </b> on line: <b> '.$myLine.' </b>';
       echo 'Error message: <b> '.$errorMsg.'</b>';
       die();
@@ -22,5 +20,5 @@ if(defined('DEBUG') && DEBUG)
     
 }
 
-include('server.php');
+
 
