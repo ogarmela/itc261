e6e6480e11ca65194e50335cb5638526d530aab6
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-25 22:30:52 -0700

    Add files via upload

---
+++ finalproject/index.php
@@ -0,0 +1,46 @@
+<?php // index.php this is our wonderful home page, that can only be reached if you login and successfully!!!
+
+session_start();
+include('config.php');
+
+if(!isset($_SESSION['UserName'])) {
+    $_SESSION['msg'] = 'You must login first';
+    header('Location:login.php');
+}
+
+if(isset($_GET['logout'])) {
+   session_destroy();
+    unset($_SESSION['UserName']);
+   header('Location:login.php');
+}
+
+ include('includes/header.php'); 
+    
+
+//Notification message
+if(isset($_SESSION['success'])) :?>
+<div class="success">
+<h3>
+<?php echo $_SESSION['success']  ;
+    unset($_SESSION['success']) ;
+    ?>     
+    </h3>
+    </div>
+    <?php endif;
+
+if(isset($_SESSION['UserName'])) :?>
+<div class="welcome-logout">
+ <h3> Hello,
+<?php echo $_SESSION['UserName'] ; ?> 
+    </h3>   
+<a href="index.php?logout='1'">Log out!</a>      
+</div>
+
+<h1>Welcome to Our home Page!</h1>
+
+
+<?php endif; ?>  
+</div>  <!---end wrapper-->
+
+<?php
+include('includes/footer.php');
