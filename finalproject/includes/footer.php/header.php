c3e341fdc621d652838cbebbf9bc39bb5e9ee4e6
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-25 22:26:34 -0700

    Add files via upload

---
+++ finalproject/includes/footer.php/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Login Page</title>
+<link href="css/styles.css" type="text/css" rel="stylesheet">  
+    
+</head>
+
+<body class="<?php echo ''.$body.''; echo $background2; ?>">
+    
+        <header>
+    <div class="inner-header">
+ <nav>
+ 
+ <ul>
+ <?php
+echo makeLinks($nav); 
+     
+ ?>                           
+ </ul>
+</nav>
+        
+        </div>
+    </header>
+
+<div id="wrapper">
