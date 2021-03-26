a9b32a622022b5a062a8cb313f6fdcc58f9ccca4
ogarmela <52357639+ogarmela@users.noreply.github.com>
2021-03-25 22:25:10 -0700

    Add files via upload

---
+++ finalproject/includes/footer.php/footer.php
@@ -0,0 +1,19 @@
+<footer>
+
+<ul>
+<li>Copyright &copy;
+           <?php
+            echo date('Y');
+              ?>
+    </li> 
+<li>All Right Reserved</li>
+<li><a href="http://omar12.com/it261/portalpages/">Portal Page</a></li>
+<li><a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fwww.omar12.com%2Fit261%2Ffinalproject%2Flogin.php">HTML VALID</a></li>
+<li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fvalidator.w3.org%2Fnu%2F%3Fdoc%3Dhttp%253A%252F%252Fwww.omar12.com%252Fit261%252Ffinalproject%252Flogin.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">CSS VALID</a></li>
+            
+            
+</ul>
+</footer>
+
+</body>
+</html>
